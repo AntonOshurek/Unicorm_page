c7f2558ab16f8a880b09880c00c21d0484d02334
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2021-12-28 17:17:59 +0100

    add scroll colors to nav

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,7 +1,9 @@
 import {buttonAnimate} from './blocks/button';
+import {navScroll} from './blocks/nav-scroll';
 
 
 window.addEventListener('DOMContentLoaded', () => {
   buttonAnimate();
+  navScroll();
 });
 
