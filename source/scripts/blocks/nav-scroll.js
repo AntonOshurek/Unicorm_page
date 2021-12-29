aec89dcf0be6c90831c26453d4007ed8b70e3f9b
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2021-12-29 11:54:17 +0100

    active checked height and scroll position on menu

--- source/scripts/blocks/nav-scroll.js
+++ source/scripts/blocks/nav-scroll.js
@@ -1,15 +1,22 @@
 const navScroll = () => {
 
   const header = document.querySelector('.header__wrap')
+  const coverBlock = document.querySelector('.cover');
+
+  const headerHeight = window.getComputedStyle(header).height.replace(/\D+/, '')
+  const coverHeight = window.getComputedStyle(coverBlock).height.replace(/\D+/, '');
+
+  console.log(coverHeight)
 
   window.onscroll = () => {
     console.log(window.scrollY)
-    if (window.scrollY > 795) {
+    if (window.scrollY > coverHeight - (headerHeight / 2)) {
       header.classList.add('header__wrap--scroll');
     } else {
       header.classList.remove('header__wrap--scroll');
     }
-};
+  };
+
 }
 
 export {navScroll}
