c02cf539aebac62cc1bd1e8ad0fd3864596ad257
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2021-12-29 12:41:18 +0100

    optimyze nav-scroll logick

--- source/scripts/blocks/nav-scroll.js
+++ source/scripts/blocks/nav-scroll.js
@@ -1,21 +1,26 @@
 const navScroll = () => {
 
-  const header = document.querySelector('.header__wrap')
+  const header = document.querySelector('.header__wrap');
   const coverBlock = document.querySelector('.cover');
 
-  const headerHeight = window.getComputedStyle(header).height.replace(/\D+/, '')
-  const coverHeight = window.getComputedStyle(coverBlock).height.replace(/\D+/, '');
+  const headerHeight = window.getComputedStyle(header).height.replace(/\D+/, '');
+  let coverHeight = window.getComputedStyle(coverBlock).height.replace(/\D+/, '');
 
-  console.log(coverHeight)
-
-  window.onscroll = () => {
-    console.log(window.scrollY)
+  const onScrollCheck = () => {
     if (window.scrollY > coverHeight - (headerHeight / 2)) {
       header.classList.add('header__wrap--scroll');
     } else {
       header.classList.remove('header__wrap--scroll');
     }
   };
+
+  const onHeightCheck = () => {
+    coverHeight = window.getComputedStyle(coverBlock).height.replace(/\D+/, '');
+    onScrollCheck();
+  };
+
+  window.addEventListener('scroll', onScrollCheck);
+  window.addEventListener('resize', onHeightCheck);
 
 }
 
