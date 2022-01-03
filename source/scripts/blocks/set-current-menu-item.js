7a57899edd2b099874f44e03e7d27ad35ead5a7b
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-01-03 18:51:27 +0100

    correction styles and js in reference

--- source/scripts/blocks/set-current-menu-item.js
+++ source/scripts/blocks/set-current-menu-item.js
@@ -4,6 +4,7 @@ const setCurrentMenuItem = () => {
   const skillsBlock = document.querySelector('.skills');
   const aboutUsBlock = document.querySelector('.about-us');
   const systemsBlock = document.querySelector('.systems');
+  const referenceBlock = document.querySelector('.reference');
   const footerBlock = document.querySelector('.footer');
 
   const navigateLinks = document.querySelectorAll('.nav__link');
@@ -12,6 +13,7 @@ const setCurrentMenuItem = () => {
   const skillsLink = document.querySelector('.nav__link--skills');
   const aboutLink = document.querySelector('.nav__link--aboutUs');
   const systemslink = document.querySelector('.nav__link--systems');
+  const referencelink = document.querySelector('.nav__link--reference');
   const footerLink = document.querySelector('.nav__link--footer');
 
   const isScrolledIntoView = (elem) => {
@@ -44,6 +46,11 @@ const setCurrentMenuItem = () => {
     if(isScrolledIntoView(systemsBlock)) {
       navigateLinks.forEach((item) => {item.classList.remove('nav__link--current')})
       systemslink.classList.add('nav__link--current');
+    }
+
+    if(isScrolledIntoView(referenceBlock)) {
+      navigateLinks.forEach((item) => {item.classList.remove('nav__link--current')})
+      referencelink.classList.add('nav__link--current');
     }
 
     if(isScrolledIntoView(footerBlock)) {
