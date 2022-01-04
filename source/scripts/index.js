caea2423edc538491baa5acf802708ee362004b6
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-01-04 16:40:53 +0100

    add srvices block

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,11 +1,13 @@
 import {navScroll} from './blocks/nav-scroll';
 import {setCurrentMenuItem} from './blocks/set-current-menu-item';
 import {mobileMenu} from './blocks/mobile-menu';
+import {showServices} from './blocks/show-services';
 
 
 window.addEventListener('DOMContentLoaded', () => {
   navScroll();
   setCurrentMenuItem();
   mobileMenu();
+  showServices();
 });
 
