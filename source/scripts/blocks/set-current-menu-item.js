eb80a99f259619fd3fc85d7421f43c8a8b3dca0f
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-01-04 11:24:31 +0100

    js code correction

--- source/scripts/blocks/set-current-menu-item.js
+++ source/scripts/blocks/set-current-menu-item.js
@@ -1,21 +1,5 @@
 const setCurrentMenuItem = () => {
 
-  const coverBlock = document.querySelector('.cover');
-  const skillsBlock = document.querySelector('.skills');
-  const aboutUsBlock = document.querySelector('.about-us');
-  const systemsBlock = document.querySelector('.systems');
-  const referenceBlock = document.querySelector('.reference');
-  const footerBlock = document.querySelector('.footer');
-
-  const navigateLinks = document.querySelectorAll('.nav__link');
-  // nav__link--current
-  const startLink = document.querySelector('.nav__link--start');
-  const skillsLink = document.querySelector('.nav__link--skills');
-  const aboutLink = document.querySelector('.nav__link--aboutUs');
-  const systemslink = document.querySelector('.nav__link--systems');
-  const referencelink = document.querySelector('.nav__link--reference');
-  const footerLink = document.querySelector('.nav__link--footer');
-
   const isScrolledIntoView = (elem) => {
     const rect = elem.getBoundingClientRect();
 
@@ -24,42 +8,31 @@ const setCurrentMenuItem = () => {
 
     const isVisible = (elemTop < (window.innerHeight / 2) && Math.sign(elemTop) != -1) || ((elemBottom > window.innerHeight / 2) && elemBottom < window.innerHeight);
     return isVisible;
-  }
+  };
 
-  window.addEventListener('scroll', () => {
+  const allSections = document.querySelectorAll('section');
+  const allNavLinks = document.querySelectorAll('.nav__link');
+  const footerBlock = document.querySelector('.footer');
 
-    if(isScrolledIntoView(coverBlock)) {
-      navigateLinks.forEach((item) => {item.classList.remove('nav__link--current')})
-      startLink.classList.add('nav__link--current');
-    }
+  const secArr = [...allSections, footerBlock];
 
-    if(isScrolledIntoView(skillsBlock)) {
-      navigateLinks.forEach((item) => {item.classList.remove('nav__link--current')})
-      skillsLink.classList.add('nav__link--current');
-    }
+  const markCurrentMenuItem = () => {
+    secArr.forEach((section) => {
+      let sectionClass;
+      if(isScrolledIntoView(section)) {
+        sectionClass = section.className;
 
-    if(isScrolledIntoView(aboutUsBlock)) {
-      navigateLinks.forEach((item) => {item.classList.remove('nav__link--current')})
-      aboutLink.classList.add('nav__link--current');
-    }
+        allNavLinks.forEach((link) => {
+          link.classList.remove('nav__link--current');
+          if(link.getAttribute('data-nav-name') === sectionClass){
+            link.classList.add('nav__link--current');
+          }
+        })
+      }
+    })
+  };
 
-    if(isScrolledIntoView(systemsBlock)) {
-      navigateLinks.forEach((item) => {item.classList.remove('nav__link--current')})
-      systemslink.classList.add('nav__link--current');
-    }
-
-    if(isScrolledIntoView(referenceBlock)) {
-      navigateLinks.forEach((item) => {item.classList.remove('nav__link--current')})
-      referencelink.classList.add('nav__link--current');
-    }
-
-    if(isScrolledIntoView(footerBlock)) {
-      navigateLinks.forEach((item) => {item.classList.remove('nav__link--current')})
-      footerLink.classList.add('nav__link--current');
-    }
-
-  });
-
+  window.addEventListener('scroll', markCurrentMenuItem);
 }
 
 export {setCurrentMenuItem};
