69d4873ef8609d2962882876fcc5bdc189a19b53
AntonOshurek <83346452+AntonOshurek@users.noreply.github.com>
2022-01-06 11:21:36 +0000

    Deploying to gh-pages from @ AntonOshurek/Unicorn_page@08234cf0a56d499ffa586d3d5a73fad437eb495a 🚀

--- js/bundle.js
+++ js/bundle.js
@@ -174,14 +174,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 const showServices = () => {
   let imageItems = [...document.querySelectorAll('.services__img-wrap')];
-  let titles = [...document.querySelectorAll('.services__text')];
   let options = {
     rootMargin: '0px',
     threshold: .2
   };
 
   let setItemActive = entries => {
-    console.log(entries);
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add('services__img-wrap--active');
@@ -190,13 +188,9 @@ const showServices = () => {
   };
 
   let observer = new IntersectionObserver(setItemActive, options);
-  imageItems.forEach((item, idx) => {
-    //  idx % 2 == 1? item.style.left = '5%' : item.style.left = '5%';
+  imageItems.forEach(item => {
     observer.observe(item);
-  }); // titles.forEach((title, idx) => {
-  //   // idx % 2 == 0? title.style.left = '45%' : title.style.left = '35%';
-  //   observer.observe(title);
-  // })
+  });
 };
 
 
