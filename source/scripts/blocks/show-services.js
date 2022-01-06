4029caaceba161799db3559982da71f197e73a64
AntonOshurek <oshurekweb@gmail.com>
2022-01-06 12:19:59 +0100

    observer correction

--- source/scripts/blocks/show-services.js
+++ source/scripts/blocks/show-services.js
@@ -1,6 +1,5 @@
 const showServices = () => {
   let imageItems = [...document.querySelectorAll('.services__img-wrap')];
-  let titles = [...document.querySelectorAll('.services__text')];
 
   let options = {
     rootMargin: '0px',
@@ -8,25 +7,19 @@ const showServices = () => {
   }
 
   let setItemActive = (entries) => {
-    console.log(entries);
-    entries.forEach(entry => {
-      if(entry.isIntersecting) {
-        entry.target.classList.add('services__img-wrap--active');
-      }
-    })
+		entries.forEach(entry => {
+			if(entry.isIntersecting) {
+				entry.target.classList.add('services__img-wrap--active');
+			}
+		})
   }
 
   let observer = new IntersectionObserver(setItemActive, options);
 
-  imageItems.forEach((item, idx) => {
-    //  idx % 2 == 1? item.style.left = '5%' : item.style.left = '5%';
+  imageItems.forEach((item) => {
     observer.observe(item);
   })
 
-  // titles.forEach((title, idx) => {
-  //   // idx % 2 == 0? title.style.left = '45%' : title.style.left = '35%';
-  //   observer.observe(title);
-  // })
 }
 
 export {showServices};
