08d8ca050071733ad3b78d713d1a7b0d17389b87
AntonOshurek <83346452+AntonOshurek@users.noreply.github.com>
2022-01-07 19:32:33 +0000

    Deploying to gh-pages from @ AntonOshurek/Unicorn_page@467b26ff92140cbb1d307d5810a6dcc285b2793f 🚀

--- js/bundle.js
+++ js/bundle.js
@@ -74,17 +74,15 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "navScroll": function() { return /* binding */ navScroll; }
 /* harmony export */ });
 const TABLET_WIDTH = 900;
+const SCROLL_OFFSET = 40;
 
 const navScroll = () => {
   const header = document.querySelector('.header__wrap');
-  const coverBlock = document.querySelector('.cover');
   const navButtonSvg = document.querySelector('.ham');
-  const headerHeight = window.getComputedStyle(header).height.replace(/\D+/, '');
-  let coverHeight = window.getComputedStyle(coverBlock).height.replace(/\D+/, '');
   let navButton = false;
 
   const onScrollCheck = () => {
-    if (window.scrollY > coverHeight - headerHeight / 2) {
+    if (window.scrollY > SCROLL_OFFSET) {
       header.classList.add('header__wrap--scroll');
 
       if (navButton) {
@@ -99,14 +97,13 @@ const navScroll = () => {
     }
   };
 
-  if (window.getComputedStyle(header).width.replace(/\D+/, '') < TABLET_WIDTH) {
-    navButton = true;
-    onScrollCheck();
-  }
-
   const onHeightCheck = () => {
-    coverHeight = window.getComputedStyle(coverBlock).height.replace(/\D+/, '');
-    onScrollCheck();
+    if (window.getComputedStyle(header).width.replace(/\D+/, '') < TABLET_WIDTH) {
+      navButton = true;
+      onScrollCheck();
+    } else {
+      navButton = false;
+    }
   };
 
   window.addEventListener('scroll', onScrollCheck);
